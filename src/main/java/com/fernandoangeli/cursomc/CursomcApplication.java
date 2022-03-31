3a50d6f5cd4521c311807c8d22f62576b170a759
Fernando Angeli <fernando_angeli@hotmail.com>
2022-03-31 15:38:43 -0300

    Criando operação de instanciação

--- src/main/java/com/fernandoangeli/cursomc/CursomcApplication.java
+++ src/main/java/com/fernandoangeli/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.fernandoangeli.cursomc;
 
+import com.fernandoangeli.cursomc.domain.Categoria;
+import com.fernandoangeli.cursomc.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
