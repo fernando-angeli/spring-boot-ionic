525032ccd9f2091a22044e2ff9099ab664436c16
Fernando Angeli <fernando_angeli@hotmail.com>
2022-04-23 09:28:05 -0300

    Ajustando e testando deleção de clientes

--- src/main/java/com/fernandoangeli/cursomc/domain/Cliente.java
+++ src/main/java/com/fernandoangeli/cursomc/domain/Cliente.java
@@ -22,7 +22,7 @@ public class Cliente {
     private String cpfOuCnpj;
     private Integer tipo;
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
