1a6177906403f7227db63191d0e30ba9f021727a
Fernando Angeli <fernando_angeli@hotmail.com>
2022-04-27 21:39:48 -0300

    Implementando autorização

--- src/main/java/com/fernandoangeli/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fernandoangeli/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.fernandoangeli.cursomc.config;
 
 import com.fernandoangeli.cursomc.security.JWTAuthenticationFilter;
+import com.fernandoangeli.cursomc.security.JWTAuthorizationFilter;
 import com.fernandoangeli.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
