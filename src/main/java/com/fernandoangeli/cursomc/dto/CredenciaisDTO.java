7aab1bf94aa942a32532f29786e6c3a2cf277ff5
Fernando Angeli <fernando_angeli@hotmail.com>
2022-04-27 21:14:16 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/fernandoangeli/cursomc/dto/CredenciaisDTO.java
@@ -0,0 +1,16 @@
+package com.fernandoangeli.cursomc.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CredenciaisDTO {
+
+    private String email;
+    private String senha;
+
+    public CredenciaisDTO(){
+    }
+
+}
---
+++ src/main/java/com/fernandoangeli/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.fernandoangeli.cursomc.services;
+
+import com.fernandoangeli.cursomc.domain.Cliente;
+import com.fernandoangeli.cursomc.repository.ClienteRepository;
+import com.fernandoangeli.cursomc.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cli = repo.findByEmail(email);
+        if(cli == null){
+            throw new UsernameNotFoundException(email);
+        }
+        return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+    }
+
+}
