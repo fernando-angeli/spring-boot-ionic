e923fda56bee110b484053010ebbf1ca293dffbd
Fernando Angeli <fernando_angeli@hotmail.com>
2022-04-30 16:48:28 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/fernandoangeli/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fernandoangeli/cursomc/config/SecurityConfig.java
@@ -42,7 +42,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/produtos/**",
-            "/categorias/**"
+            "/categorias/**",
+            "/clientes**",
+            "/clientes/email**"
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
