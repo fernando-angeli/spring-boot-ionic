2e6132bef5848b2a7597edde1f4a1e93eb34624b
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-18 20:48:10 -0300

    Documentação com Swagger

--- src/main/java/com/fernandoangeli/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fernandoangeli/cursomc/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -71,6 +72,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**");
     }
 
     @Bean
--- src/main/java/com/fernandoangeli/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fernandoangeli/cursomc/resources/CategoriaResource.java
@@ -3,6 +3,9 @@ package com.fernandoangeli.cursomc.resources;
 import com.fernandoangeli.cursomc.domain.Categoria;
 import com.fernandoangeli.cursomc.dto.CategoriaDTO;
 import com.fernandoangeli.cursomc.services.CategoriaService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +25,14 @@ public class CategoriaResource {
     @Autowired
     private CategoriaService service;
 
+    @ApiOperation(value="Busca por id")
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
     public ResponseEntity<Categoria> find(@PathVariable Integer id){
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @ApiOperation(value="Insere categoria")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
@@ -37,6 +42,7 @@ public class CategoriaResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @ApiOperation(value="Atualiza categoria")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
@@ -46,6 +52,10 @@ public class CategoriaResource {
         return ResponseEntity.noContent().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Não é possível excluir uma categoria que possui produtos"),
+            @ApiResponse(code = 404, message = "Código inexistente") })
+    @ApiOperation(value="Deleta uma categoria")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id){
@@ -53,6 +63,7 @@ public class CategoriaResource {
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value="Retorna todas as categorias")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<CategoriaDTO>> findAll(){
         List<Categoria> list = service.findAll();
@@ -60,6 +71,7 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(listDTO);
     }
 
+    @ApiOperation(value="Retorna as categorias paginadas")
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<CategoriaDTO>> findPage(
             @RequestParam(value="page", defaultValue = "0") Integer page,
